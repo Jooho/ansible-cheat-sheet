18266aafc01e4a31201e45c322a4f8c7bea62d23
root <root@dhcp181-112.gsslab.rdu2.redhat.com>
2017-09-18 18:58:23 -0400

    v0.9 for deploy_type=ocp operate=start/stop/teardown/suspend

--- ansible-playbooks/ansible-playbook-deploy-ocp-on-rhev/test.sh
+++ ansible-playbooks/ansible-playbook-deploy-ocp-on-rhev/test.sh
@@ -20,3 +20,8 @@ ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml -
 # bg_upgrade version 2
 # ora --provider=rhev --deploy_type=bg_upgrade  --upgrade_version=3.5 --var_file="/path/to/file" --tag="35-0912"
 # ansible-playbook playbooks/config.yaml  -M  /root/ansible/lib/ansible/modules/packaging/os/  -vv --forks=12 -i /etc/ansible/hosts   -e @/path/to/file -i a.yaml -e deploy_type=bg_upgrade -e new_infra_node_vms=3 -e new_app_node_vms=2 -e ocp_version=3.5
+
+
+# scaledown
+# ansible-playbook playbooks/config.yaml  -M  ./modules -vvvv --forks=12 -i inventory/rhev/hosts/a.py  -e @vars/all  -e deploy_type=stop -e target_vm=app  -e provider=rhev -e @vars/ocp_params 
+# ansible-playbook playbooks/config.yaml  -M  ./modules -vvvv --forks=12 -i inventory/rhev/hosts/a.py  -e @vars/all  -e deploy_type=scaledown -e target_vm=infra -e provider=rhev -e @vars/ocp_params
